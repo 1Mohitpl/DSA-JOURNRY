d4f689ed09658f7675740ae0cc082f52a32ef94b
Mohit kumar paul <mohit1paul@gmail.com>
2024-03-14 00:51:43 +0530

    create stack

--- Stackes/first.cpp
+++ Stackes/first.cpp
@@ -1,17 +1,124 @@
 #include<iostream>
-#include<stack>
 using namespace std;
+
+ class stack {
+    public :
+     
+      int *arr;
+      int size ;
+      int top; 
+
+
+      stack (int size) {
+           arr = new [int size] ;
+           this->size = size;
+           this->top = -1;
+             
+      }
+
+      void push (int data) {
+        if ( top == size -1) {
+
+          cout << "stack is overflow" << endl;
+          return;
+
+        }
+        else {
+             top ++ ;
+          arr [top] = data; 
+        }
+        
+      }
+
+      void pop () {
+         if (top == -1) {
+
+           cout << "stack is underflow" << endl;
+           return;
+
+         }
+         else{
+             
+             top --;
+         }
+         
+      }
+
+      bool isempty () {
+          if (top == -1) {
+               return true;
+          }
+          else {
+                return false;
+          }
+      }
+
+      int getTop () {
+
+          if (top == -1 ) {
+
+               cout << "stack is empty" << endl;
+               return -1;
+          } 
+          else{
+                
+                  return arr [top] ;
+
+          }
+          
+
+      }
+ };
+
 int main () {
 
-     // stack creation
-
-     stack<int>st;
-
-     // stack insertion 
-
-     st.push(20);
-     st.push(50);
-     st.push(69);
-
-     cout << "size of the given stack" << st.size() << endl;
+     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include<iostream>
+// #include<stack>
+// using namespace std;
+// int main () {
+
+//      // stack creation
+
+//      stack<int>st;
+
+//      // stack insertion 
+
+//      st.push(20);
+//      st.push(50);
+//      st.push(69);
+
+//      cout << "size of the given stack" << st.size() << endl;
+
+
+//      st.pop();   // stack remove
+ 
+//           cout << "size of the given stack" << st.size() << endl;
+// }
