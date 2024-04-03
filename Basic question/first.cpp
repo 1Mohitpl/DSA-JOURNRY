0a264508779a451a448dc933f34ec5a538eec027
Mohit kumar paul <mohit1paul@gmail.com>
2024-04-03 22:47:50 +0530

    another pattern question

--- Basic question/first.cpp
+++ Basic question/first.cpp
@@ -1,20 +1,77 @@
 #include<iostream>
 using namespace std;
 
-int main () {
+int main  () {
 
-     int n ;
-     cout << "Enter an number : " << endl;                  // pattern print  
-     cin >> n;                                              
+     int n;
+     cout << "Enter an value of an N : " << endl;
+     cin >> n;
 
      for (int i = 0; i <= n; i++)
      {
-        for (int j = 0; j < i+1; j++)
+        for  (int j = 0; j < n-i; j++)
         {
             cout << "*";
+        
 
-        }  
+        }
         cout << endl;
      }
      
-}     
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main () {
+
+//      int n ;
+//      cout << "Enter an number : " << endl;                  // pattern print  
+//      cin >> n;                                              
+
+//      for (int i = 0; i <= n; i++)
+//      {
+//         for (int j = 0; j < i+1; j++)
+//         {
+//             cout << "*";
+
+//         }  
+//         cout << endl;
+//      }
+     
+// }     
