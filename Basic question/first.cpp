6686451b5ee9ef3a0df6466d23b2b59e5f75e5fb
Mohit kumar paul <mohit1paul@gmail.com>
2024-04-04 19:09:22 +0530

    Hollow full piramid

--- Basic question/first.cpp
+++ Basic question/first.cpp
@@ -3,27 +3,86 @@ using namespace std;
 
 int main () {
 
-   int n; 
-   cout << "Enter an value of an N : " << endl;
-   cin >> n;
+     int n;
+     
+     cout << "Enter an value of an N : " << endl;
+      cin >> n;
 
-   for (int i = 0; i <n; i++)
-   {
-      for (int j = 0; j < n; j++)
-      {
-        if (i==0 || i == n-1 || j==0 || j==n-1)
-        {
-          cout << "*";
-        }else{
-           cout << " ";
-        }
-        
-      }
-      
-      cout << endl;
-   }
-   
+     for (int i = 0; i < n; i++)
+     {       int k = 0;
+       for (int j = 0; j <((2*n)-1); j++)
+       {   
+          if (j<n-i-1)
+          {
+             cout << " ";
+
+          }else if(k < 2 * i +1){
+            cout << "*";
+            k++;
+          } else{
+             cout << " ";
+          }
+          
+       }
+       cout << endl;
+     }
+     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main () {
+
+//    int n; 
+//    cout << "Enter an value of an N : " << endl;
+//    cin >> n;
+
+//    for (int i = 0; i <n; i++)
+//    {
+//       for (int j = 0; j < n; j++)
+//       {
+//         if (i==0 || i == n-1 || j==0 || j==n-1)
+//         {
+//           cout << "*";
+//         }else{
+//            cout << " ";
+//         }
+        
+//       }
+      
+//       cout << endl;
+//    }
+   
+// }
 
 
 
