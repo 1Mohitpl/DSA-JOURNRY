08aaee65525adfd4a8f3dd20f102f3c9c5cec336
Mohit kumar paul <mohit1paul@gmail.com>
2024-04-04 19:23:58 +0530

    full piramid

--- Basic question/first.cpp
+++ Basic question/first.cpp
@@ -1,33 +1,86 @@
 #include<iostream>
-using namespace std;
+using  namespace std;
 
 int main () {
 
      int n;
-     
      cout << "Enter an value of an N : " << endl;
-      cin >> n;
+     cin >> n;
 
-     for (int i = 0; i < n; i++)
-     {       int k = 0;
-       for (int j = 0; j <((2*n)-1); j++)
-       {   
-          if (j<n-i-1)
-          {
-             cout << " ";
+     for (int row = 0; row < n; row++)
+     {
+       for (int col = 0; col < n-row-1; col++)
+       {
+          cout << " ";
 
-          }else if(k < 2 * i +1){
-            cout << "*";
-            k++;
-          } else{
-             cout << " ";
-          }
-          
+       }
+       
+       for (int col  = 0; col  < row+1; col ++)
+       {
+          cout << "* ";
        }
        cout << endl;
      }
      
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main () {
+
+//      int n;
+     
+//      cout << "Enter an value of an N : " << endl;
+//       cin >> n;
+
+//      for (int i = 0; i < n; i++)
+//      {       int k = 0;
+//        for (int j = 0; j <((2*n)-1); j++)             
+//        {   
+//           if (j<n-i-1)
+//           {
+//              cout << " ";
+
+//           }else if(k < 2 * i +1){
+//             cout << "*";
+//             k++;
+//           } else{
+//              cout << " ";
+//           }
+          
+//        }
+//        cout << endl;
+//      }
+     
+// }
 
 
 
