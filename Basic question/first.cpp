f1753666cbe2271705314dd6ea770447756b6293
Mohit kumar paul <mohit1paul@gmail.com>
2024-04-04 18:52:28 +0530

    hollow square

--- Basic question/first.cpp
+++ Basic question/first.cpp
@@ -1,24 +1,81 @@
 #include<iostream>
 using namespace std;
 
-int main  () {
+int main () {
 
-     int n;
-     cout << "Enter an value of an N : " << endl;
-     cin >> n;
+   int n; 
+   cout << "Enter an value of an N : " << endl;
+   cin >> n;
 
-     for (int i = 0; i <= n; i++)
-     {
-        for  (int j = 0; j < n-i; j++)
+   for (int i = 0; i <n; i++)
+   {
+      for (int j = 0; j < n; j++)
+      {
+        if (i==0 || i == n-1 || j==0 || j==n-1)
         {
-            cout << "*";
+          cout << "*";
+        }else{
+           cout << " ";
+        }
+        
+      }
+      
+      cout << endl;
+   }
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main  () {
+
+//      int n;
+//      cout << "Enter an value of an N : " << endl;
+//      cin >> n;
+
+//      for (int i = 0; i <= n; i++)
+//      {
+//         for  (int j = 0; j < n-i; j++)
+//         {
+//             cout << "*";
         
 
-        }
-        cout << endl;
-     }
+//         }
+//         cout << endl;
+//      }
      
-}
+// }
 
 
 
