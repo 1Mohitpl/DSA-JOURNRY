38ad3b031b46bbf29995103302083aa4e0d7757f
Mohit kumar paul <mohit1paul@gmail.com>
2024-04-09 19:01:56 +0530

    git commit

--- Basic question/01_Solution.cpp
+++ Basic question/01_Solution.cpp
@@ -1,20 +1,120 @@
-#include <iostream>
-using namespace std;
+#include<iostream>
+using namespace std ;
 
-int main()
-{
+int main () {
 
-    int num;
-    cout << "Enter an number : " << endl;              //sum of even numbers in the given number
-    cin >> num;
-    int sum = 0;
-    
-    for (int i = 0; i <= num; i++)
+    int n ;
+    cout << "Enter an value of N :" << endl;
+
+    cin  >> n;
+    for (int i = 0; i < n; i++)                                 // Numeric half hollow peramid
     {
-        if (i % 2 == 0)
+        for (int  j= 0; j < i+1; j++)
         {
-            sum = sum + i;
+            if ( j== 0 || j == i || i == n-1)
+            {
+                cout << j+1;
+            } else{
+                 cout << " ";
+            }
+            
         }
+        cout << endl;
     }
-    cout << "Sum of even numbers in the given number is  :" << sum << endl;
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include<iostream>
+// using namespace std ;
+
+// int main () {
+
+//      int n;
+//      cout << "Enter an value of an N: " << endl;
+//      cin >> n;
+//                                                                         // Numeric half piramid 
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < i +1; j++)
+
+//         {
+//             cout << j +1;
+            
+//         }
+//         cout << endl;
+//     }
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+
+//     int num;
+//     cout << "Enter an number : " << endl;              //sum of even numbers in the given number
+//     cin >> num;
+//     int sum = 0;
+    
+//     for (int i = 0; i <= num; i++)
+//     {
+//         if (i % 2 == 0)
+//         {
+//             sum = sum + i;
+//         }
+//     }
+//     cout << "Sum of even numbers in the given number is  :" << sum << endl;
+// }
+
+
