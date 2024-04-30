bcc5fc212f10158936fa33152aef5b46ce5875a5
Mohit kumar paul <mohit1paul@gmail.com>
2024-04-30 21:19:41 +0530

    git commit

--- Basic/arrays.cpp
+++ Basic/arrays.cpp
@@ -9,7 +9,7 @@ using namespace std ;
      int onecount = 0;
 
      for (int i = 0; i < size; i++)
-     {                                                                 // count  "0" and "1" in an array 
+     {                                                                 // count  "0" and "1" in an array
          if (arr[i] == 0)
          {
             zerocount++;
