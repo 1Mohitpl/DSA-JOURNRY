fe208186311872f7bb449e6ab727426cfbc9aa09
Mohit kumar paul <mohit1paul@gmail.com>
2024-05-06 23:14:20 +0530

    key-pair

--- Leetcode/Remove_duplicate.cpp
+++ Leetcode/Remove_duplicate.cpp
@@ -9,3 +9,26 @@
          
          return j+1;
         
+
+
+
+// ++++++++++++++++++++++++++++++++ key pair +++++++++++++++++ 
+
+
+bool twoSumapproach(int arr[], int n, int x){
+        int l =0;
+        int h= n-1;
+        while(l<h){
+            
+           int sum = arr[l] + arr[h];
+           if(sum == x) return true;
+           else if(sum > x) h--;
+           else l++;
+        }
+       return false;
+    }
+	bool hasArrayTwoCandidates(int arr[], int n, int x) {
+	    
+	     sort(arr, arr+n);
+	     return twoSumapproach(arr, n,x);
+	}
