94fc246b3b0f513c1864886b479f7eca85e0cf34
Mohit kumar paul <mohit1paul@gmail.com>
2024-05-09 01:53:11 +0530

    tow sum problem

---
+++ Leetcode/missing elements from an array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace sdt;
+
+
+void findMissing(int arr[], int n){
+
+    
+}
+int main() {
+
+    int n;
+    int arr[] = {1,3,4,3,5};
+    int n = sizeof(a) / sizeof(int); // find out the size of an array
+
+    findMissing(arr, n);
+
+}
---
+++ Leetcode/two_sum.cpp
@@ -0,0 +1,10 @@
+int n = nums.size();
+        for(int i = 0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(nums[i] + nums[j] == target){
+                    return {i,j};
+                }
+            }
+        }
+         return {-1,-1};
+    }
