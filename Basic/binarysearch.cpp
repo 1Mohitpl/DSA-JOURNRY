8b5f830564f70b1876799943a63e016a72a02964
Mohit kumar paul <mohit1paul@gmail.com>
2024-05-29 21:51:06 +0530

    find first occurence in the array

--- Basic/binarysearch.cpp
+++ Basic/binarysearch.cpp
@@ -30,7 +30,7 @@ int binarysearch(int arr[], int n, int target) {
 
 int main() {
     int arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90};
-    int target = 90;
+    int target = 50;
     int n = sizeof(arr) / sizeof(arr[0]);
 
     int targetResult = binarysearch(arr, n, target);
@@ -39,4 +39,59 @@ int main() {
     } else {
         cout << "Target element found at index " << targetResult << endl;
     }
+}
+
+
+//++++++++++++++++++++++++++++find firsr first occurence in the given array
+
+
+
+#include<iostream>
+using namespace std;
+
+
+
+int findFirstoccurence(int arr[], int n, int target ){
+
+    int start  = 0;
+    int end = n-1;
+    int mid = start +(end-start)/2;   // best practice 
+    int ans = -1;
+    while (start<=end){
+         int mid = (start+end)/2;
+        if (arr[mid] == target)
+        {
+            ans = mid;
+            // left shit
+            end  = mid-1;
+        } else if (arr[mid] < target)
+        {
+            start = mid+1;
+        } else if (arr[mid]>target)
+        {
+              end = mid-1;
+        }
+        
+        
+        
+    }
+    return ans;
+    
+}
+
+
+
+
+
+
+int main () {
+
+    int arr[] = {10,20,30,30,30,30,40,50,50,60,70};
+    int n = 11;
+    int target = 50;
+
+    int result = findFirstoccurence(arr,n,target);
+    cout << "first occurence: " << result << endl;
+
+
 }
