b081830892842478029b63255906c7e0222ab98a
Mohit kumar paul <mohit1paul@gmail.com>
2024-06-09 23:03:56 +0530

    recursion in array

--- Recursion/main.cpp
+++ Recursion/main.cpp
@@ -2,22 +2,115 @@
 using namespace std;
 
 
-int pow(int n){
-    if(n == 0){
-        return 1;
-                                                                  // pow of a number
-    } else{
-        return 2 * pow(n-1);           
-    }
+void printArray(int arr[], int n, int index){
+
+      //base case 
+      if(index >=n){
+        return ;
+      }
+
+     cout << arr[index] << " ";
+
+     // recursive call
+
+     printArray(arr, n, index+1);
 }
-int main (){
-int n;
-cout << "Enter your nuber : " << endl;
-cin >>n;
-int ans = pow(n);
-cout << ans << endl;
 
 
 
+
+int main(){
+ int arr[] = {10,20,30,40,50};
+ int n = 5;
+
+ int index = 0;
+  printArray(arr, n, index);
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+
+
+// int pow(int n){
+//     if(n == 0){
+//         return 1;
+//                                                                   // pow of a number
+//     } else{
+//         return 2 * pow(n-1);           
+//     }
+// }
+// int main (){
+// int n;
+// cout << "Enter your nuber : " << endl;
+// cin >>n;
+// int ans = pow(n);
+// cout << ans << endl;
+
+
+
+
+// }
