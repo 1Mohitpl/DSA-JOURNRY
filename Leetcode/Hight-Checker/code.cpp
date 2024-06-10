efe22e1d3b3cbc5b5453f645f6d7d065c6549e3a
Mohit kumar paul <mohit1paul@gmail.com>
2024-06-10 23:11:25 +0530

    hight checker problem

---
+++ Leetcode/Hight-Checker/code.cpp
@@ -0,0 +1,13 @@
+int heightChecker(vector<int>& heights) {
+        vector<int>expected = heights;
+        int count =0;
+        sort(expected.begin(),expected.end());
+        for(int i=0; i<heights.size(); i++){
+            if(expected[i]!= heights[i]){
+                count ++;
+
+            }
+            
+        }
+        return count;
+    }
