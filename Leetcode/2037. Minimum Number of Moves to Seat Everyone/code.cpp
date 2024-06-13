c076c0829adb42f539a6f126a3c5cff75c712a00
Mohit kumar paul <mohit1paul@gmail.com>
2024-06-13 23:50:15 +0530

    solve daily poroblem

---
+++ Leetcode/2037. Minimum Number of Moves to Seat Everyone/code.cpp
@@ -0,0 +1,15 @@
+int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+             sort(seats.begin(), seats.end());
+             sort(students.begin(), students.end());
+
+             int moves = 0;
+             int n = seats.size();
+             for(int i = 0; i<n; i++){
+                moves += abs(seats[i]- students[i]);
+
+             }
+           return moves;
+           
+
+
+    }
