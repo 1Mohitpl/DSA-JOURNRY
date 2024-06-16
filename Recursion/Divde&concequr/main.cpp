8edcff332d1b5dbafbcd3d53966c93161f48e822
Mohit kumar paul <mohit1paul@gmail.com>
2024-06-16 19:10:35 +0530

    dive and conquer

---
+++ Recursion/Divde&concequr/main.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+void merge(int arr[], int start, int end){
+    
+}
+
+void mergsort(int arr[], int start, int end,){
+    //base case
+    if(start>= end) {
+        return;
+         
+    }
+
+    // break the problem into two small problem 
+    int mid = start + (end-start)/2;
+    
+    // now the porblem is breaks into two parts once is left part, another part is right part
+
+    //for the left part
+    mergsort(arr, s,mid);
+    // for the right part
+    mergsort(arr, mid+1, end);
+
+
+}
+
+int main (){
+
+    int arr[] = {2,1, 6,9, 4,5};
+    int size = 6;
+    int start = 0;
+    int end = size-1;
+
+    margsort(arr, start, end,)
+}
