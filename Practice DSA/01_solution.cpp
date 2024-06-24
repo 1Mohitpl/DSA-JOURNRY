8dab8cd8a599f133a5dd5c3e0766bb26a6fa0027
Mohit kumar paul <mohit1paul@gmail.com>
2024-06-24 11:21:37 +0530

    pratice dsa

--- Practice DSA/01_solution.cpp
+++ Practice DSA/01_solution.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 void reverse(string&s, int p1, int p2){
-      
+
       //base case
       if (p1>= p2)
       {
@@ -13,7 +13,6 @@ void reverse(string&s, int p1, int p2){
 
       swap(s[p1], s[p2]);
       reverse(s, p1+1, p2-1);
-      
 
 }
 
@@ -26,257 +25,171 @@ int main(){
    cout << s << endl;
 }
 
-
-
-
-
-// #include<iostream>
-// #include<vector>
-// using namespace std;
-
-
-//   void solve(int arr[], int size , int index, vector<int>&anseven, vector<int>&ansOdd){
-
-//     if(index >= size){
-//         return;
-//     }
-
-//      if(arr[index] %2==0){
-//         anseven.push_back(arr[index]);
-//      }
-//      if(arr[index]%2==1){
-//         ansOdd.push_back(arr[index]);
-//      }
-
-//      solve(arr, size, index+1, anseven, ansOdd);
-
-//   }
-// int main (){
-//     int arr[] = {10, 2, 15, 20,29,27};
-//     int size = 6;
-//     int index = 0;
-    
-//     vector<int>anseven;
-//     vector<int>ansOdd;
-
-//     solve(arr, size, index, anseven, ansOdd);
-
-//     // print ans using foreach loop
-
-//     for(int num: anseven ){
-//         cout << "Even numbers : " << num << " ";
-//     }
-//      cout << endl;
-//    for(int num : ansOdd){
-//     cout << "Odd Numbers :" << num << " ";
-//    }
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// #include <iostream>
-
-// using namespace std;
-
-// bool checkprimeornot(int n)
-// { // check the number is prime number or not a prime number
-
-//     if (n == 2)
-//     {
-//         return true;
-//     }
-
-//     if (n % 2 == 0)
-//     {
-//         return false;
-//     }
-//     else
-//     {
-//         return true;
-//     }
-// }
-
-// int main()
-// {
-
-//     int n;
-//     cout << "Enter a number here : " << endl;
-//     cin >> n;
-//     bool ans = checkprimeornot(n);
-
-//     if (ans == 0)
-//     {
-//         cout << "Not a Prime nuber" << endl;
-//     }
-//     else
-//     {
-//         cout << "prime number" << endl;
-//     }
-// }
-
-// #include <iostream>
-// using namespace std;
-
-// bool checkNumber(int n)
-// {
-
-//     if (n % 2 == 0)
-//     { // check the number is even or odd
-//         return true;
-//     }
-//     else
-//     {
-//         return false;
-//     }
-// }
-
-// int main()
-// {
-
-//     int n;
-//     cout << "Enter an numer here : " << endl;
-//     cin >> n;
-
-//     bool ans = checkNumber(n);
-
-//     if (ans == 1)
-//     {
-//         cout << "Number is even" << endl;
-//     }
-//     else
-//     {
-//         cout << "Number is odd" << endl;
-//     }
-// }
-
-// #include <iostream>
-// using namespace std;
-
-// int sumofEvennumbers(int n)
-// {
-//     int sum = 0;
-//     for (int i = 2; i <= n; i++)
-//     { // sum of all even numbers 1 to N
-//         if (i % 2 == 0)
-//         {
-//             sum = sum + i;
-//         }
-//     }
-//     return sum;
-// }
-
-// int main()
-// {
-
-//     int n;
-//     cout << "Enter an number here : " << endl;
-//     cin >> n;
-//     int ans = sumofEvennumbers(n);
-//     cout << "sum of all even numbers : " << ans << endl;
-// }
-
-// #include <iostream>
-// using namespace std;
-
-// int sumofnumbers(int n)
-// {
-//     int sum = 0;
-//     for (int i = 0; i <= n; i++)
-//     {
-//         sum = sum + i;
-//     }
-//     return sum;
-// }
-
-// // sum of all numbers upto N
-
-// int main()
-// {
-
-//     int n;
-//     cout << "Enter an number : " << endl;
-//     cin >> n;
-
-//     int ans = sumofnumbers(n);
-//     cout << "sum of all numbers upto N : " << ans << endl;
-// }
+#include<iostream>
+#include<vector>
+using namespace std;
+
+  void solve(int arr[], int size , int index, vector<int>&anseven, vector<int>&ansOdd){
+
+    if(index >= size){
+        return;
+    }
+
+     if(arr[index] %2==0){
+        anseven.push_back(arr[index]);
+     }
+     if(arr[index]%2==1){
+        ansOdd.push_back(arr[index]);
+     }
+
+     solve(arr, size, index+1, anseven, ansOdd);
+
+  }
+int main (){
+    int arr[] = {10, 2, 15, 20,29,27};
+    int size = 6;
+    int index = 0;
+
+    vector<int>anseven;
+    vector<int>ansOdd;
+
+    solve(arr, size, index, anseven, ansOdd);
+
+    // print ans using foreach loop
+
+    for(int num: anseven ){
+        cout << "Even numbers : " << num << " ";
+    }
+     cout << endl;
+   for(int num : ansOdd){
+    cout << "Odd Numbers :" << num << " ";
+   }
+
+}
+
+#include <iostream>
+
+using namespace std;
+
+bool checkprimeornot(int n)
+{ // check the number is prime number or not a prime number
+
+    if (n == 2)
+    {
+        return true;
+    }
+
+    if (n % 2 == 0)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+int main()
+{
+
+    int n;
+    cout << "Enter a number here : " << endl;
+    cin >> n;
+    bool ans = checkprimeornot(n);
+
+    if (ans == 0)
+    {
+        cout << "Not a Prime nuber" << endl;
+    }
+    else
+    {
+        cout << "prime number" << endl;
+    }
+}
+
+#include <iostream>
+using namespace std;
+
+bool checkNumber(int n)
+{
+
+    if (n % 2 == 0)
+    { // check the number is even or odd
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+
+    int n;
+    cout << "Enter an numer here : " << endl;
+    cin >> n;
+
+    bool ans = checkNumber(n);
+
+    if (ans == 1)
+    {
+        cout << "Number is even" << endl;
+    }
+    else
+    {
+        cout << "Number is odd" << endl;
+    }
+}
+
+#include <iostream>
+using namespace std;
+
+int sumofEvennumbers(int n)
+{
+    int sum = 0;
+    for (int i = 2; i <= n; i++)
+    { // sum of all even numbers 1 to N
+        if (i % 2 == 0)
+        {
+            sum = sum + i;
+        }
+    }
+    return sum;
+}
+
+int main()
+{
+
+    int n;
+    cout << "Enter an number here : " << endl;
+    cin >> n;
+    int ans = sumofEvennumbers(n);
+    cout << "sum of all even numbers : " << ans << endl;
+}
+
+#include <iostream>
+using namespace std;
+
+int sumofnumbers(int n)
+{
+    int sum = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+// sum of all numbers upto N
+
+int main()
+{
+
+    int n;
+    cout << "Enter an number : " << endl;
+    cin >> n;
+
+    int ans = sumofnumbers(n);
+    cout << "sum of all numbers upto N : " << ans << endl;
+}
--- Practice DSA/check palindrom/code.cpp
+++ Practice DSA/check palindrom/code.cpp
@@ -1,49 +1,148 @@
-#include <iostream>
+// check the string is palindrom or not using RE
+#include<iostream>
 using namespace std;
 
-// difin function
+bool ispalindrom(string&s, int start, int end) {
 
-bool ispalindrom(int x)
-{
-    if (x < 0)
-    {
-        return false;
-    }
-
-    int temp;
-    temp = x;
-    int reverseNum = 0;
-    int digit;
-    while (x > 0)
-    {
-        digit = x % 10;                          // find it last digit
-        reverseNum = (reverseNum * 10) + digit; // put that digit into the reversenum
-        x = x/ 10;
-    }
-
-    if (temp == reverseNum)
-    {
+    if(start>=end){
         return true;
-    } else{
-        return false;
     }
+
+    if (s[start] != s[end])
+    {
+         return false;
+    }
+     return ispalindrom(s, start+1, end-1);
+}
+
+int main (){
+
+    string s;
+    cout << "Enter an string" << endl;
+    cin >>s;
+   cout << ispalindrom(s, 0, s.size()-1) << endl;
+  
+     
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// // difin function
+
+// bool ispalindrom(int x)
+// {
+//     if (x < 0)
+//     {
+//         return false;
+//     }
+
+//     int temp;
+//     temp = x;
+//     int reverseNum = 0;
+//     int digit;
+//     while (x > 0)
+//     {
+//         digit = x % 10;                          // find it last digit
+//         reverseNum = (reverseNum * 10) + digit; // put that digit into the reversenum
+//         x = x/ 10;
+//     }
+
+//     if (temp == reverseNum)
+//     {
+//         return true;
+//     } else{
+//         return false;
+//     }
     
    
-}
+// }
 
-int main()
-{
-    int x;
-    cout << "Enter an input number :" << endl;
-    cin >> x;
+// int main()
+// {
+//     int x;
+//     cout << "Enter an input number :" << endl;
+//     cin >> x;
 
-  int ans = ispalindrom(x);
-    if (ans == 1)
-    {
-        cout << "the number is palindrom" << endl;
-    }
-    else
-    {
-        cout << "Number is not a palindrom number" << endl;
-    }
-}
+//   int ans = ispalindrom(x);
+//     if (ans == 1)
+//     {
+//         cout << "the number is palindrom" << endl;
+//     }
+//     else
+//     {
+//         cout << "Number is not a palindrom number" << endl;
+//     }
+// }
