b68a714cb1b4fb2d3b6a65d31320c261546f1675
Mohit kumar paul <mohit1paul@gmail.com>
2024-06-27 02:07:25 +0530

    first program of linked list

--- Linked-List/main.cpp
+++ Linked-List/main.cpp
@@ -5,11 +5,25 @@ class Node{
     public:
      int data;
      Node* next;    // next is a pointer that point to the node
-     
+
+
+     Node(){
+        cout << "default ctor is called" << endl;
+        this->next=NULL;
+     }
+
+     Node(int data){
+        cout << "parameterose ctor is called" << endl;
+        this->data = data;
+        this->next= NULL; 
+     }
 
 };
 
 int main () {
+   Node*next;
 
+   Node* first = new Node(10);
+   Node* second = new Node(20);
 
 }
