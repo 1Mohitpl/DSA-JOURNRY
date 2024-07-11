b4985b0dde7105695737eab160c04e54d3464d55
Mohit kumar paul <mohit1paul@gmail.com>
2024-07-11 19:50:44 +0530

    deletion in link-list

--- Linked-List/main2.cpp
+++ Linked-List/main2.cpp
@@ -1,126 +1,124 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Node{
-    public:
+class Node {
+public:
     int val;
-    Node* pointer;                // create node
-    
+    Node* pointer;
 
-    Node(){
-        cout << "default construatore is called" <<endl;
+    Node() {
+        cout << "Default constructor is called" << endl;
         this->pointer = NULL;
     }
 
-    Node(int val){
-        // cout << "parametarised constractor is called" << endl;
+    Node(int val) {
         this->val = val;
         this->pointer = NULL;
     }
+
+    ~Node() {
+        cout << "Destructor is called for node with value " << this->val << endl;
+    }
 };
 
- void  printall (Node* head){
-        Node* temp = head; // create an temporray pointer 
+void printAll(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->val << " ";
+        temp = temp->pointer;
+    }
+    cout << endl;
+}
 
-        while (temp != NULL)
-        {
-             cout << temp->val  << " ";
-             temp = temp ->pointer;
-        }
-      cout << endl;  
+int getLength(Node* head) {
+    Node* temp = head;
+    int count = 0;
+    while (temp != NULL) {
+        count++;
+        temp = temp->pointer;
+    }
+    return count;
+}
 
+void insertAtHead(Node*& head, Node*& tail, int val) {
+    Node* newNode = new Node(val);
+    if (head == NULL) {
+        head = tail = newNode;
+    } else {
+        newNode->pointer = head;
+        head = newNode;
+    }
+}
 
+void insertAtTail(Node*& head, Node*& tail, int val) {
+    Node* newNode = new Node(val);
+    if (head == NULL) {
+        head = tail = newNode;
+    } else {
+        tail->pointer = newNode;
+        tail = newNode;
+    }
+}
+
+void createTail(Node*& head, Node*& tail, int val) {
+    Node* temp = head;
+    while (temp->pointer != NULL) {
+        temp = temp->pointer;
+    }
+    tail = temp;
+}
+
+void deleteNode(Node*& head, Node*& tail, int position) {
+    if (head == NULL) {
+        cout << "Cannot delete, list is empty" << endl;
+        return;
     }
 
-    int getlenght(Node* head) {
+    int len = getLength(head);
+
+    if (position == 1) {
         Node* temp = head;
-        int count = 0;
-        while (temp != NULL)
-        {
-             count ++;
-             temp = temp ->pointer;
+        head = head->pointer;
+        delete temp;
+    } else if (position == len) {
+        Node* prev = head;
+        while (prev->pointer != tail) {
+            prev = prev->pointer;
         }
-
-        return count;
-        
-    }
-
-    void insertAthead(Node* &head, Node* &tail, int val){
-        if(head == NULL){
-            // ceate an new node
-            Node* newnode = new Node(val);
-            //  updating head 
-
-            head = newnode;
-            tail = newnode;
-        } else{
-            Node* temp = new Node(val);
-        temp->pointer = head;
-        head = temp;
-
+        delete tail;
+        tail = prev;
+        tail->pointer = NULL;
+    } else {
+        Node* prev = NULL;
+        Node* curr = head;
+        for (int i = 1; i < position; i++) {
+            prev = curr;
+            curr = curr->pointer;
         }
-
-        
+        prev->pointer = curr->pointer;
+        delete curr;
     }
+}
 
-    void insertedAttail(Node* &head, Node* &tail, int val){
-        if(head == NULL){
-            // create an node
-            Node* newnode = new Node(val);
-            head= newnode;
-            tail = newnode;
-        } else{
-            Node* newnode = new Node(val);
-            tail->pointer = newnode;
-            tail = newnode;
-        }
-    }
-
-    void createTail(Node* &head, Node* &tail, int val){
-        // create an temp pointer
-
-        Node* temp = head;
-        while(temp ->pointer != NULL){
-            temp =  temp->pointer;
-        }
-
-        tail = temp;
-    }
-
-int main (){
-    //creation of node
-    //  Node a;    // static allocation 
-    // Node* head1 = new Node(10);    // dynamically creation of an node
-    // Node* head2 = new Node(20);  
-    // Node* head3 = new Node(30);  
-    // Node* head4 = new Node(40);  
-    // Node* head5 = new Node(50);  
-    // Node* head6 = new Node(60);  
-    // Node* head7 = new Node(70);  
-    // Node* head8 = new Node(80);  
-
-    // head1->pointer = head2;
-    // head2->pointer = head3;
-    // head3->pointer = head4;
-    // head4->pointer = head5;
-    // head5->pointer = head6;
-    // head6->pointer = head7;
-    // head7->pointer = head8;
-    // head8->pointer = NULL;       // creation of linked  list 
-
-    Node* head  = NULL;
+int main() {
+    Node* head = NULL;
     Node* tail = NULL;
-  
 
+    insertAtHead(head, tail, 10);
+    insertAtHead(head, tail, 20);
+    insertAtHead(head, tail, 30);
+    insertAtHead(head, tail, 40);
+    insertAtHead(head, tail, 50);
+    insertAtHead(head, tail, 60);
 
-    insertAthead(head, tail, 10);
-    insertAthead(head,tail, 20);
-    insertAthead(head,tail, 30);
-    insertAthead(head,tail, 40);
-    insertAthead(head,tail,50);
-    insertAthead(head,tail, 60);
+    insertAtTail(head, tail, 400);
 
-   insertedAttail(head, tail, 400);
-     printall(head);
-        cout << "lenght" << getlenght(head);
+    printAll(head);
+    cout << "Length: " << getLength(head) << endl;
+
+    deleteNode(head, tail, 1);
+    printAll(head);
+    cout << "Length: " << getLength(head) << endl;
+
+    return 0;
 }
