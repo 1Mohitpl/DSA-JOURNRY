183c8bb56bdb90f603eccc7996a4545d4766637d
Mohit kumar paul <mohit1paul@gmail.com>
2024-07-31 09:47:04 +0530

    previousSmaller element

--- Stackes/next smaller element.cpp
+++ Stackes/next smaller element.cpp
@@ -1,42 +1,84 @@
+// #include<iostream>
+// #include<stack>
+// #include<vector>
+// using namespace std;
+
+// vector<int> nextsmallerelement(int *arr, int size, vector<int> &ans){
+//     //create an stack
+//     stack<int>st;
+//     // initially stack's element is -1
+//     st.push(-1);
+
+//     for (int i = size-1; i >=0; i--)
+//     {
+//         int curr = arr[i];
+//         while (st.top() >= curr)
+//         {
+//             st.pop();
+//         }
+//          ans[i] = st.top();  // store the ans;
+//          st.push(curr);
+        
+//     }
+
+//     return ans; 
+    
+// }
+
+// int main(){
+//   int arr[] = {8, 4, 6, 2, 3};
+//   int size = 5;
+//   vector<int> ans(size);  //for access the index you have to give size as an argument
+  
+//   ans = nextsmallerelement(arr, size, ans);
+
+//   for(auto i : ans){
+//     cout << i << " "; 
+//       }
+
+//       cout << endl;
+//    return 0;
+
+// }
+
+
+// previous smaller element
+
 #include<iostream>
 #include<stack>
 #include<vector>
 using namespace std;
 
-vector<int> nextsmallerelement(int *arr, int size, vector<int> &ans){
-    //create an stack
-    stack<int>st;
-    // initially stack's element is -1
-    st.push(-1);
+vector<int> previousSmallerElement(int *arr, int size, vector<int> &ans){
 
-    for (int i = size-1; i >=0; i--)
-    {
+  stack<int>st;
+  st.push(-1);
+
+  for (int i = 0; i <=size-1; i++){
         int curr = arr[i];
         while (st.top() >= curr)
         {
-            st.pop();
+           st.pop();
         }
-         ans[i] = st.top();  // store the ans;
-         st.push(curr);
         
-    }
+        ans[i] = st.top();
+        st.push(curr);
+  }
 
-    return ans; 
-    
+  return ans;
+  
 }
 
-int main(){
-  int arr[] = {8, 4, 6, 2, 3};
-  int size = 5;
-  vector<int> ans(size);  //for access the index you have to give size as an argument
-  
-  ans = nextsmallerelement(arr, size, ans);
+int main (){
+    int arr[] = {8,4,6,2,3};
+    int size = 5;
+    vector<int>ans(size);
+    ans = previousSmallerElement(arr, size, ans);
+    for(auto i: ans){
+      cout << i << " ";
 
-  for(auto i : ans){
-    cout << i << " "; 
-      }
+    }
 
-      cout << endl;
-   return 0;
+    cout << endl;
 
 }
