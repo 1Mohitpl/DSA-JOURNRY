a7652ec5547e7d1e99c17be0f497dde5962e7e32
Mohit kumar paul <mohit1paul@gmail.com>
2024-09-25 00:41:21 +0530

    class2

--- Heap/class-2/code.cpp
+++ Heap/class-2/code.cpp
@@ -1,19 +1,131 @@
 #include<iostream>
 #include<queue>
+
 using namespace std;
 
+int main () {
+    int arr[] = {3,7,4,5}
+}
 
-int main(){
-    // create a max heap 
 
-    priority_queue<int>pq;
-    pq.push(10);
-    pq.push(30);
-    pq.push(20);
 
-    cout << "top element : " << pq.top() << endl;
-    pq.pop();
-    cout << "top element : " << pq.top() << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include<iostream>
+// #include<queue>
+// using namespace std;
+
+
+// int main(){
+//     // create a max heap 
+
+//     priority_queue<int>pq;
+//     pq.push(10);
+//     pq.push(30);
+//     pq.push(20);
+
+//     cout << "top element : " << pq.top() << endl;
+//     pq.pop();
+//     cout << "top element : " << pq.top() << endl;
+
+
+// // min-heap creation
+// priority_queue<int, vector<int>, greater<int>>pq;
+  
     
 
-}
+
+// }
