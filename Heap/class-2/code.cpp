98dac0725631c48fa2c4ba852ed8ebe63a0756a4
Mohit kumar paul <mohit1paul@gmail.com>
2024-09-24 21:13:05 +0530

    heap class2

---
+++ Heap/class-2/code.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+
+int main(){
+    // create a max heap 
+
+    priority_queue<int>pq;
+    pq.push(10);
+    pq.push(30);
+    pq.push(20);
+
+    cout << "top element : " << pq.top() << endl;
+}
