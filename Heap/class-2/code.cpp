f3cebfa215fc306afa49f151a5feed8702907481
Mohit kumar paul <mohit1paul@gmail.com>
2024-09-28 01:17:33 +0530

    greatestkth element

--- Heap/class-2/code.cpp
+++ Heap/class-2/code.cpp
@@ -1,10 +1,74 @@
 #include<iostream>
 #include<queue>
-
 using namespace std;
 
+
+int getKthsmllestElement(int arr[], int n, int k){
+      priority_queue<int> pq;
+    // process the first k element from the array
+    for(int i = 0; i<k; i++){
+        int element = arr[i];  // take each element 
+       pq.push(element);
+    }
+
+    // push remening element if laser than top element
+    for( int i =k; i<n; i++){
+        int element = arr[i];
+        if(element < pq.top()){
+            // delete that top element 
+            pq.pop();
+            // now push the current element
+            pq.push(element);
+
+        }
+
+    }
+
+    // now fetch the kth element from the top
+    int ans = pq.top();
+    return ans;
+}
+
+
+
+
+int getKthgreatestElement(int arr[], int n, int k){
+    priority_queue<int, vector<int>, greater<int>>pq;
+    // process the first k element from the array
+    for(int i = 0; i<k; i++){
+        int element = arr[i];  // take each element 
+       pq.push(element);
+    }
+
+    // push remening element if laser than top element
+    for( int i =k; i<n; i++){
+        int element = arr[i];
+        if(element > pq.top()){
+            // delete that top element 
+            pq.pop();
+            // now push the current element
+            pq.push(element);
+
+        }
+
+    }
+
+    // now fetch the kth element from the top
+    int ans = pq.top();
+    return ans;
+}
+
 int main () {
-    int arr[] = {3,7,4,5}
+    int arr[] = {3,5, 4, 6, 9, 8, 7};
+    int n = 7;
+    int k = 3;
+    int result1 = getKthsmllestElement(arr, n, k);
+    cout << result1 <<endl;
+
+    int result2 = getKthgreatestElement(arr, n, k);
+    cout << result2 <<endl;
+    
+
 }
 
 
