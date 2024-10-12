8216ecc986f6a7241af6b447785519486ffa60a8
Mohit kumar paul <mohit1paul@gmail.com>
2024-10-12 22:26:48 +0530

    get length of the array

--- Practice DSA/strings/code.cpp
+++ Practice DSA/strings/code.cpp
@@ -1,10 +1,27 @@
 #include<iostream>
 using namespace std;
 
+int findlength(char ch[], int size){
+    int length = 0;
+     for(int i = 0; i< size;i++){
+        if(ch[i] == '\0'){
+            break;
+        } else {
+            length++;
+        }
+     }
+     return length;
+}
+
 
 int main (){
-     char ch[10];
-     cin >> ch;
+     char ch[100];
+     cin >> ch;   // single line of taking input 
+    //  cin.getline(ch, 100);
+    
+    int len = findlength(ch, 100);
 
-     cout << "printing the character array : " << ch <<endl;
+
+     cout << "printing the character array : " << ch <<endl; 
+     cout << "length of the array :" << len << endl;  // not need of for loop for printing array
 }
