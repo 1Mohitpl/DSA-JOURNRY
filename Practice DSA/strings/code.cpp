3e1d7b9acf7b29d09c487e2a161330b371634c0f
Mohit kumar paul <mohit1paul@gmail.com>
2024-10-13 14:00:03 +0530

    convertUpper string

--- Practice DSA/strings/code.cpp
+++ Practice DSA/strings/code.cpp
@@ -13,15 +13,40 @@ int findlength(char ch[], int size){
      return length;
 }
 
+void reverseString (char ch[], int n){
+    int i = 0;
+    int j = n-1;
+    while(i<=j){
+        swap(ch[i], ch[j]);
+        i++;
+        j--;
+    }
+}
+
+void convertToUpper(char ch[], int n ){
+    int index = 0;
+    while(ch[index] != '\0'){
+        ch[index] = ch[index] -'a'+ 'A';
+        index++;
+    }
+}
 
 int main (){
      char ch[100];
-     cin >> ch;   // single line of taking input 
-    //  cin.getline(ch, 100);
+    //  cin >> ch;   // single line of taking input 
+    cout << "Enter an string : " << endl;
+     cin.getline(ch, 100);
     
-    int len = findlength(ch, 100);
+   
 
 
-     cout << "printing the character array : " << ch <<endl; 
-     cout << "length of the array :" << len << endl;  // not need of for loop for printing array
+    //  cout << "printing the character array : " << ch <<endl; 
+    //  cout << "length of the array :" << len << endl;  // not need of for loop for printing array
+//    cout << "before revrse the string" << " " << ch << endl;
+//     int len = findlength(ch, 100);
+//    reverseString(ch, len);
+//    cout << "after reverse the string " << ch <<endl;
+
+   convertToUpper(ch, 100);
+   cout << ch << endl;
 }
