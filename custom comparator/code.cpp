db0ac9818d066ad389bb8a6c524077026f3738d8
Mohit kumar paul <mohit1paul@gmail.com>
2024-10-16 18:07:00 +0530

    custom sort string

---
+++ custom comparator/code.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+ bool mycomp (int &a , int &b ){
+    return a >b;
+ }
+int main (){
+
+    // vector<int> st = {10,11, 22, 15, 14};
+    //    sort(st.begin(), st.end(), mycomp);
+
+    //  for(int i = 0; i < st.size(); i++){
+    //      int val = st[i];
+    //      cout << val << " ";
+    //  }
+
+    /* vector of vector sorting*/
+
+    vector<vector<int>>v;
+}
