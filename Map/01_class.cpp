0eb3453b32e0c94eeb06377f3faea00236f3659c
Mohit kumar paul <mohit1paul@gmail.com>
2024-10-17 21:45:54 +0530

    count freqency of character of a string

--- Map/01_class.cpp
+++ Map/01_class.cpp
@@ -3,27 +3,54 @@
 
 using namespace std;
 
+void countCharacters(unordered_map<char, int> &mapping, string ch){
+      for (auto temp : ch) {
+           mapping[temp]++;
+}
+                                // now increae the frequency of each characters
+        
+}
+
 int main (){
 
-    // creation of map
-    unordered_map<string, int>map;
+//     // creation of map
+//     unordered_map<string, int>map;
 
-    // insertion 
-    pair<string, int> p = make_pair("mohit", 23);
+//     // insertion 
+//     pair<string, int> p = make_pair("mohit", 23);
 
-    pair<string, int>q("ram", 24);
+//     pair<string, int>q("ram", 24);
 
-    pair<string, int>r;
-    r.first = "sneha";
-    r.second = 23;
+//     pair<string, int>r;
+//     r.first = "sneha";
+//     r.second = 23;
 
-    map.insert(p);
-    map.insert(q);
-    map.insert(r);
+//     map.insert(p);
+//     map.insert(q);
+//     map.insert(r);
 
-    cout << "size of map :" << map.size() << endl;
-    cout << map.at("mohit") << endl;
+//     cout << "size of map :" << map.size() << endl;
+//     cout << map.at("mohit") << endl;
     
-    // searching
-    cout << map.count("mohit") << endl;
+//     // searching
+//     cout << map.count("mohit") << endl;
+
+//     // search key in map
+
+//     if(map.find("sneha") != map.end()){
+//         cout << "found" << endl;
+//     } else {
+//         cout << "not found" << endl;
+//     }
+
+     string ch = "mohit";
+     unordered_map<char, int > mapping;  // create map
+
+     countCharacters(mapping, ch);
+   
+     // foreach mapping
+     for(auto val : mapping){
+         
+         cout << val.first<< val.second <<  endl;
+     }
 }
