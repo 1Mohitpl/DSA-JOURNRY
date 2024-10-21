22e7e384627cab36e51d07b0b006d6f9310579f0
Mohit kumar paul <mohit1paul@gmail.com>
2024-10-21 20:36:52 +0530

    longest palindrom subtring

---
+++ Leetcode/Longest Palindromic Substring/code.cpp
@@ -0,0 +1,23 @@
+bool ispalindrom(string &s, int start, int end){
+       while(start<end){
+         if(s[start] != s[end]){
+            return false;
+          
+         }
+           start++;
+            end--;
+       }
+       return true;
+   }
+    string longestPalindrome(string s) {
+        string ans = "";
+        for(int i=0; i<s.size(); i++){
+            for(int j = i; j<s.size(); j++){
+                if(ispalindrom(s, i, j)){
+                    string t = s.substr(i, j-i+1);
+                    ans = t.size() > ans.size() ? t : ans;
+                }
+            }
+        }
+        return ans;
+    }
