1a16aa4c50fb10658053dc24da03d20caf246a64
Mohit kumar paul <mohit1paul@gmail.com>
2024-10-25 12:13:45 +0530

    dynamic array

--- Practice DSA/STL/vector.cpp
+++ Practice DSA/STL/vector.cpp
@@ -8,7 +8,18 @@ void printarr(int arr[], int n){
      cout << endl;
 }
 int main () {
-      int arr[5] = {1, 2, 3, 5,6};   // static memeory allocation
-      int n = 5;
+    //   int arr[5] = {1, 2, 3, 5,6};   // static memeory allocation
+    /* lets make it dynamic memory allocation*/
+    int n;
+    cin >> n;
+    int *arr = new int[n];
+
+    // taking n elements as inputs  and put in array 
+    for(int i = 0; i<n; i++){
+        int data ;
+        cin >> data;
+        arr[i] = data;
+    }
       printarr(arr, n);
+    
 }
