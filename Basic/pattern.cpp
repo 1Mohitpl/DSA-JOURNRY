d62ade395113d8feea576a040c8a3d62ec9fa637
Mohit kumar paul <mohit1paul@gmail.com>
2024-11-10 18:29:27 +0530

    sum of n

--- Basic/pattern.cpp
+++ Basic/pattern.cpp
@@ -2,38 +2,117 @@
 using namespace std;
 
 int main () {
-    int n;
-    cout << "Enter an value of n :" << endl;
-    cin >> n;
-    int c = 1;
-    // growing phase
-    for(int i = 0; i<n; i++){
-        for(int j = 0; j<=i; j++){
-            cout <<c;
-              c++;
-              if(j<i){
-                cout << "*";
-              }
-            
-        }
-     cout << endl;
-     }
-     int start = c-n;
-     // lower phase
-      for(int i = 0; i<n; i++){
-          int m = start;
-            for(int j = 0; j<=n-i-1;j++){
-                cout << m;
-                m++;
-                if(j<n-i-1){
-                    cout << "*";
-                }
-            }
-            start = start-(n-i-1);
-             cout << endl;
-        }
-       
+  int n = 5;
+  int sum = 0;
+  for(int i= 1; i<=n; i++){
+      sum+=i;
+    
+  }
+    cout <<sum;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main () {
+//     int n;
+//     cout << "Enter an value of n :" << endl;
+//     cin >> n;
+//     int c = 1;
+//     // growing phase
+//     for(int i = 0; i<n; i++){
+//         for(int j = 0; j<=i; j++){
+//             cout <<c;
+//               c++;
+//               if(j<i){
+//                 cout << "*";
+//               }
+            
+//         }
+//      cout << endl;
+//      }
+//      int start = c-n;
+//      // lower phase
+//       for(int i = 0; i<n; i++){
+//           int m = start;
+//             for(int j = 0; j<=n-i-1;j++){
+//                 cout << m;
+//                 m++;
+//                 if(j<n-i-1){
+//                     cout << "*";
+//                 }
+//             }
+//             start = start-(n-i-1);
+//              cout << endl;
+//         }
+       
+// }
 
 
 
