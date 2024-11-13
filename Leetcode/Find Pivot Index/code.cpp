5a943c7e94331d91be7639d5bd532402667fd20b
Mohit kumar paul <mohit1paul@gmail.com>
2024-11-13 14:51:38 +0530

    find pivot index

---
+++ Leetcode/Find Pivot Index/code.cpp
@@ -0,0 +1,23 @@
+// bruteforece approach 
+
+     int leftrightSum (vector<int>& nums){
+       
+          for(int i = 0; i<nums.size(); i++){
+               int leftsum = 0;
+                int rightsum = 0;
+              for(int j = 0; j<i; j++)
+                  leftsum += nums[j];
+
+                  for(int k = i+1; k<nums.size(); k++)
+                     rightsum += nums[k];
+                  
+
+               if(leftsum == rightsum)
+              return i;
+          }
+          return -1;
+     }
+    int pivotIndex(vector<int>& nums) {
+    
+         return leftrightSum(nums);
+    }
