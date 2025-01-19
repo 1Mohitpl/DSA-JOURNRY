1ea541b3149a67ccdd196a7c3efa80e5d2afcc0b
Mohit kumar paul <mohit1paul@gmail.com>
2025-01-20 00:00:54 +0530

    minim difference

---
+++ Leetcode/Minimum Absolute Difference/code.cpp
@@ -0,0 +1,18 @@
+ vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        
+        sort(arr.begin(), arr.end());
+        vector<vector<int>>ans;
+
+        int mini = INT_MAX;
+
+        for(int i = 0; i<arr.size() -1; i++){
+             mini = min(mini, arr[i+1] - arr[i]);
+        }
+
+        for(int i = 0; i<arr.size()-1; i++){
+             if(arr[i+1] - arr[i] == mini){
+                ans.push_back({arr[i], arr[i+1]});
+             }
+        }
+        return ans;
+    }
