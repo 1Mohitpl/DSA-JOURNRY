ba124e3685b71b47551cdcef61b2df1a79c3637e
Mohit kumar paul <mohit1paul@gmail.com>
2025-01-27 01:02:53 +0530

    solve using tabulation method

--- Dynamic programming class/main.cpp
+++ Dynamic programming class/main.cpp
@@ -21,3 +21,30 @@ int solvememo(vector<int>&dp, int n){
         int ans = solvememo(dp,n);
         return ans;
     }
+
+/*solving using tabulation method (bottom-up approach)*/
+
+ 
+    int solvtebu(int n ){
+        // create an dp array
+        vector<int>dp(n+1, -1);
+        // assign base from analysis the problem
+        dp[0] = 0;
+        if(n==0){
+            return 0;
+        }
+
+        dp[1] = 1;
+        // now we can store remaning in dp array using loop
+        for(int idx = 2; idx<=n; idx++){
+            dp[idx] = dp[idx-1] + dp[idx-2];
+        }
+ 
+        return dp[n];
+    }
+   
+    int fib(int n) {
+        int ans = solvtebu(n);
+        return ans;
+    }
+
