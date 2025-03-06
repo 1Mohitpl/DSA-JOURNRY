46a005668b7a23df3277e82acb692159381bac7d
Mohit kumar paul <mohit1paul@gmail.com>
2025-03-06 14:11:11 +0530

    uisng hasmap

--- Leetcode/find the duplicate number.cpp
+++ Leetcode/find the duplicate number.cpp
@@ -7,3 +7,19 @@ sort(nums.begin(), nums.end());   // using sorting approach here, after that tri
             }
         }
         return -1;
+
+        // using Set
+    
+        int findDuplicate(vector<int>& nums) {
+            unordered_set<int>st;
+            int  n = nums.size();
+   
+            for(int i = 0; i<n; i++){
+                 if(st.find(nums[i]) != st.end()){
+                        // measn preasent;
+                  return nums[i];
+                 } 
+                 st.insert(nums[i]);
+            }
+            return -1;
+       }
