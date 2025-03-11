8df26022d89cb962f488fa23ac4611d5047d2e8b
Mohit kumar paul <mohit1paul@gmail.com>
2025-03-11 23:49:08 +0530

    leetcode daily problem

---
+++ Leetcode/Number of Substrings Containing All Three Characters/code.cpp
@@ -0,0 +1,27 @@
+int numberOfSubstrings(string s) {
+    int n = s.length();
+
+    int result = 0;
+    
+    vector<int> mp(3, 0); //0 - a, 1 - b, 2 - c
+
+    int i = 0;
+    int j = 0;
+    while(j < n) {
+        char ch = s[j];
+        mp[ch-'a']++;
+
+        while(mp[0] > 0 && mp[1] > 0 && mp[2] > 0) {
+            result += (n - j);
+
+            mp[s[i] - 'a']--;
+            i++;
+        }
+
+        j++;
+    }
+
+    return result;
+
+    
+}
