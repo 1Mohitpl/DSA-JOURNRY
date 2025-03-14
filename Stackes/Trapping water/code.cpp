cdbf458cee3bc136595485385cf5a67638dce9ad
Mohit kumar paul <mohit1paul@gmail.com>
2025-03-14 14:07:09 +0530

    better approach

--- Stackes/Trapping water/code.cpp
+++ Stackes/Trapping water/code.cpp
@@ -1,4 +1,5 @@
-int trap(vector<int>& height) {
+// Water trapping Problem ; Brute force apporch
+int trap(vector<int>& height) { 
     int n = height.size();
     int trapwater = 0;
 
@@ -17,4 +18,31 @@ int trap(vector<int>& height) {
         trapwater += min(leftmax, rightmax) - height[i];
     }
     return trapwater;
+}
+
+
+// better approach which gives you tiem complexty of O(N)
+
+int trap(vector<int>& height) {
+    int n = height.size();
+    vector<int>lmax(n,0);
+    vector<int>rmax(n,0);
+    lmax[0] = height[0];
+    rmax[n-1] = height[n-1];
+   
+
+    for(int i = 1; i<n; i++){
+        lmax[i] = max(lmax[i-1], height[i]);
+    }
+
+     for(int i = n-2; i>=0; i--){
+         rmax[i] = max(rmax[i+1], height[i]);
+     }
+     int ans = 0;
+    for(int i = 0; i<n; i++){
+         ans += min(lmax[i], rmax[i]) - height[i];
+    }
+    return ans;
+
+
 }
