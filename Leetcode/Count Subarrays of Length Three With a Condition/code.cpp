6c5171754f6a3c0ecc499425b3355fa0c5082fc0
Mohit kumar paul <mohit1paul@gmail.com>
2025-04-27 13:19:52 +0530

    leetcode daily problem

---
+++ Leetcode/Count Subarrays of Length Three With a Condition/code.cpp
@@ -0,0 +1,10 @@
+int countSubarrays(vector<int>& nums) {
+    int n = nums.size();
+    int count = 0;
+    for(int i = 1; i<n-1; i++){
+        if(nums[i] == (nums[i-1] + nums[i+1])*2){
+            count++;
+        }
+    }
+    return count;
+}
