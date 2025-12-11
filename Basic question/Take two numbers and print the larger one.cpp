0135e7c4836bf3ba8ac776169ea66b4383d861be
Mohit kumar paul <mohit1paul@gmail.com>
2025-12-11 21:33:29 +0530

    Take two numbers and print the larger one

---
+++ Basic question/Take two numbers and print the larger one.cpp
@@ -0,0 +1,24 @@
+//Take two numbers and print the larger one
+
+#include<iostream>
+using namespace std;
+
+int main () {
+
+    int number1;
+    int number2;
+    cout << "Enter an Number1:" << endl;
+    cin >> number1;
+    cout << "Enter an Number2:" << endl;
+    cin >> number2;
+
+    if(number1 > number2) {
+        cout << number1 << endl;
+    } else {
+        cout << number2 << endl;
+    }
+
+
+
+
+}
